9c74998fc22125155c14f5e03ceec307f9f88e92
Rodo-Gala <Rodolphe.davidg@gmail.com>
2024-01-19 15:24:18 +0100

    Test 7 CI

--- tests/InMemoryStorageTest.php
+++ tests/InMemoryStorageTest.php
@@ -2,9 +2,11 @@
 
 namespace App\Tests;
 
+use App\InMemoryStorage;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes;
 
+#[Attributes\CoversClass(InMemoryStorage::class)]
 class InMemoryStorageTest extends TestCase
 {
     public function test(): void
